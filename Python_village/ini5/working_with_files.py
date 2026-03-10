a4a84e8d803cedd260724225f097c514048f29fc
Paredes0 <noeparedesalf@gmail.com>
2026-03-10 16:39:56 +0100

    Solve INI5: Working with Files

---
+++ Python_village/ini5/working_with_files.py
@@ -0,0 +1,34 @@
+"""
+Problem
+
+Given: A file containing at most 1000 lines.
+
+Return: A file containing all the even-numbered lines from the original file. Assume 1-based numbering of lines.
+
+Sample Dataset:
+
+Bravely bold Sir Robin rode forth from Camelot
+Yes, brave Sir Robin turned about
+He was not afraid to die, O brave Sir Robin
+And gallantly he chickened out
+He was not at all afraid to be killed in nasty ways
+Bravely talking to his feet
+Brave, brave, brave, brave Sir Robin
+He beat a very brave retreat
+
+Sample Output:
+
+Yes, brave Sir Robin turned about
+And gallantly he chickened out
+Bravely talking to his feet
+He beat a very brave retreat
+"""
+
+def even_lines (file: str) -> str:
+
+    with open(file) as f:
+        lines = f.read().strip().split("\n")
+    
+    return "\n".join(lines[1::2])
+
+print(even_lines("rosalind_ini5.txt"))
